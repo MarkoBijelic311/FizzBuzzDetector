45ec202e5978cfca533af65706207c4df84686a2
Marko Bijelic <bijelicmarko311@gmail.com>
2026-07-31 16:02:02 +0200

    fix: convention update

--- FizzBuzzDetector.cs
+++ FizzBuzzDetector.cs
@@ -13,16 +13,19 @@ public class FizzBuzzDetector {
         string[] words = text.Split(" ");
 
         if (text == null) {
+
             Console.WriteLine("The input text is null.");
             return;
         }
 
         if (text.Length < 7 || text.Length > 100) {
+
             Console.WriteLine("The input text must be between 7 and 100 characters.");
             return;
         }
 
         if (!text.Any(char.IsLetterOrDigit)) {
+
             Console.WriteLine("Input text does not contain any letters or digits.");
             return;
         }
@@ -37,29 +40,29 @@ public class FizzBuzzDetector {
             _wordCount++;
 
             if (_wordCount % 15 == 0) {
+
                 _newSentence.Add("FizzBuzz");
                 _fizzBuzzCount++;
-            }
-            
-            else if (_wordCount % 3 == 0) {
+            } else if (_wordCount % 3 == 0) {
+
                 _newSentence.Add("Fizz");
                 _fizzBuzzCount++;
-            }
+            } else if (_wordCount % 5 == 0) {
 
-            else if (_wordCount % 5 == 0) {
                 _newSentence.Add("Buzz");
                 _fizzBuzzCount++;
-            }
+            } else {
 
-            else {
                 _newSentence.Add(word);
             }
 
         }
 
-        foreach(string word in _newSentence) {
+        foreach (string word in _newSentence) {
+
             Console.Write(word + " ");
         }
+
         Console.WriteLine("\n\ncount: " + _fizzBuzzCount);
     }
 }
